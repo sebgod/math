54a59ebff3f5d52922fd3be9538a53a26b91c1af
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2015-01-09 21:31:01 +0800

    test_l_system.m: add 4 more examples

--- test_l_system.m
+++ test_l_system.m
@@ -27,24 +27,147 @@
 
 :- import_module l_system.
 
+:- import_module char.
 :- import_module list.
+:- import_module string.
 :- import_module require.
+:- import_module solutions.
+
+%----------------------------------------------------------------------------%
+%
+%   Example grammars from Wikipedia
+%
+
+:- type turtle ---> turtle.
+
+:- type turtle_func(T) == (func(l_system.result(T)) = turtle).
+:- inst turtle_func == (func(in) = (out) is det).
+
+:- some [T] pred example(int, string, grammar(T), turtle_func(T)).
+:- mode example(in, out, out(unique), out(turtle_func)) is semidet.
+:- mode example(out, out, out(unique), out(turtle_func)) is multi.
+
+%----------------------------------------------------------------------------%
+
+example(1, "Algea",
+    grammar(['A', 'B'], [], ['A'],
+        (func(V) =
+            ( V = 'A' -> ['A', 'B']
+            ; V = 'B' -> ['A']
+            ; [V] % this is implied by L-systems
+            )
+        )
+    ),
+    (func(_) = turtle)
+).
+
+%----------------------------------------------------------------------------%
+
+example(2, "Pythagoras Tree",
+    grammar(['0', '1'], ['[', ']'], ['0'],
+        (func(V) =
+            ( V = '1' -> ['1', '1']
+            ; V = '0' -> ['1', '[', '0', ']', '0']
+            ; [V] % this is implied by L-systems
+            )
+        )
+    ),
+    (func(_) = turtle)
+).
+
+%----------------------------------------------------------------------------%
+
+example(3, "Cantor dust",
+    grammar(['A', 'B'], [], ['A'],
+        (func(V) =
+            ( V = 'A' -> ['A', 'B', 'A']
+            ; V = 'B' -> ['B', 'B', 'B']
+            ; [V] % this is implied by L-systems
+            )
+        )
+    ),
+    (func(_) = turtle)
+).
+
+%----------------------------------------------------------------------------%
+
+example(4, "Koch curve",
+    grammar(['F'], ['+', '-'], ['F'],
+        (func(V) =
+            ( V = 'F' -> ['F', '+', 'F', '-', 'F', '-', 'F', '+', 'F']
+            ; [V] % this is implied by L-systems
+            )
+        )
+    ),
+    (func(_) = turtle)
+).
+
+%----------------------------------------------------------------------------%
+
+example(5, "Sierpinksi triangle",
+    grammar(['A', 'B'], ['+', '-'], ['A'],
+        (func(V) =
+            ( V = 'A' -> ['B', '-', 'A', '-', 'B']
+            ; V = 'B' -> ['A', '+', 'B', '+', 'A']
+            ; [V] % this is implied by L-systems
+            )
+        )
+    ),
+    (func(_) = turtle)
+).
+
+%----------------------------------------------------------------------------%
+
+example(6, "Dragon curve",
+    grammar(['X', 'Y'], ['F', '+', '-'], ['F', 'X'],
+        (func(V) =
+            ( V = 'X' -> ['X', '+', 'Y', 'F', '+']
+            ; V = 'Y' -> ['-', 'F', 'X', '-', 'Y']
+            ; [V] % this is implied by L-systems
+            )
+        )
+    ),
+    (func(_) = turtle)
+).
 
 %----------------------------------------------------------------------------%
 
 main(!IO) :-
-    OrigGrammar =
-        grammar(['A', 'B'], [], ['A'],
-            (func(V) =
-                ( V = 'A' -> ['A', 'B']
-                ; V = 'B' -> ['A']
-                ; unexpected($file, $pred, "unknown variable")
-                )
-            )
-        ),
-    produce_all(OrigGrammar, 7, [], RevResults),
-    reverse(RevResults, Results),
-    foldl(io.write_line, Results, !IO).
+    ExampleNumbers =
+        solutions((pred(Number::out) is multi :- example(Number, _, _, _))),
+    foldl(print_example, ExampleNumbers, !IO).
+
+:- pred print_example(int::in, io::di, io::uo) is det.
+
+print_example(Number, !IO) :-
+    ( if
+        example(Number, Name, Grammar, Turtle)
+    then
+        io.format("https://en.wikipedia.org/wiki/L-system#Example_%d:_%s\n",
+            [i(Number), s(replace_all(Name, " ", "_"))], !IO),
+        produce_all(Grammar, 3, [], RevResults),
+        reverse(RevResults, Results),
+        foldl(print_as_line, Results, !IO),
+        nl(!IO)
+    else
+        sorry($file, $pred, format("Example %d", [i(Number)]))
+    ).
+
+%----------------------------------------------------------------------------%
+%
+% Printing utilities
+%
+
+:- pred print_as_line(list(T)::in, io::di, io::uo) is det.
+
+print_as_line(List, !IO) :-
+    write_string("> ", !IO),
+    ( dynamic_cast(List, Chars) ->
+        write_string(from_char_list(Chars), !IO)
+    ;
+        write(List, !IO)
+    ),
+    nl(!IO).
 
 %----------------------------------------------------------------------------%
 :- end_module test_l_system.
